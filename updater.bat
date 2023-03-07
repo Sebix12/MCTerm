98afc6c71333f0148dd73cca67f62e944c44de1b
Sebix12 <93684044+Sebix12@users.noreply.github.com>
2023-03-07 23:35:14 +0100

    Add files via upload

--- updater.bat
+++ updater.bat
@@ -1,3 +1,21 @@
 @echo off
 title MCTerm FS installer
 ::-----------------------
+set ghconsole=https://raw.githubusercontent.com/Sebix12/MCTerm/main/console.bat
+set ghhelp=https://raw.githubusercontent.com/Sebix12/MCTerm/main/help.db
+set ghtsvc=https://raw.githubusercontent.com/Sebix12/MCTerm/main/tsvc.bat
+title install script : %ghconsole%, %ghhelp%, %ghtsvc%
+if exist console.bat del console.bat
+if exist help.db del help.db
+if exist tsvc.bat del tsvc.bat
+timeout /t 3 /nobreak > NUL
+echo --------------------
+start /wait curl --output console.bat --ssl-no-revoke "%ghconsole%"
+echo downloaded console
+start /wait curl --output help.db --ssl-no-revoke "%ghhelp%"
+echo downloaded help database
+start /wait curl --output tsvc.bat --ssl-no-revoke "%ghtsvc%"
+echo downloaded termsvc
+echo update complete!
+timeout /t 5
+exit
