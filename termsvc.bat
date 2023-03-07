ef30a61134090147bb4ae717bd3105f96c0d0776
Sebix12 <93684044+Sebix12@users.noreply.github.com>
2023-03-07 21:49:30 +0100

    Add files via upload

---
+++ termsvc.bat
@@ -0,0 +1,8 @@
+@echo off
+cd svcbin
+if "%1"=="server start config" call startsvcconf.bat
+if "%1"=="server start" call startsvc.bat
+if "%1"=="" goto :
+if "%1"=="" goto :
+if "%1"=="" goto :
+if "%1"=="" goto :
